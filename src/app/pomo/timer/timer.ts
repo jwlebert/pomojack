f5be5417666a4a683899df5fd0d4d3acc3b3c277
jwlebert <j.lebert4402@gmail.com>
2025-08-28 16:25:13 -0400

    setup a simple timer, can only start

--- src/app/pomo/timer/timer.ts
+++ src/app/pomo/timer/timer.ts
@@ -1,45 +1,77 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, Pipe, PipeTransform, signal, WritableSignal } from '@angular/core';
 
-interface TimeRecord {
-  start?: number; // epoch
-  remaining: number;
+// class TimeRecord {
+//   duration: number;
+//   remaining: number;
+//   public elapsed = computed(() => this.duration - this.remaining);
+//   segments: [Date, Date?][];
+
+//   public constructor(duration = 20 * 60) {
+//     this.duration = duration;
+//     this.remaining = duration;
+//     this.segments = [];
+//   }
+
+//   public startSegment() {
+//     this.segments = [...this.segments, [new Date(), undefined]];
+//   }
+
+//   public endSegment() {
+
+//   }
+// }
+
+@Pipe({ name: 'TimerDisplayPipe'})
+export class TimerDisplayPipe implements PipeTransform {
+  transform(value: number): string {
+    const h = Math.floor(value / 3600);
+    const hSeg = h > 0 ? `${h}:` : '';
+
+    const m = Math.floor((value % 3600) / 60);
+    const mStr = ('00' + m).slice(-2);
+    
+    const s = value % 60;
+    const sStr = ('00' + s).slice(-2);
+    
+    return `${hSeg}${mStr}:${sStr}`;
+  }
 }
 
 @Component({
   selector: 'app-timer',
-  imports: [],
+  imports: [TimerDisplayPipe],
   templateUrl: './timer.html',
 })
 
 export class Timer {
   public active = signal(false);
-  private times: WritableSignal<TimeRecord[]> = signal([
-    {remaining: 20 * 60}
-  ])
+  public duration = 20 * 60;
+  private start = signal(new Date());
 
-  private now = signal(Date.now());
-
-
+  private now = signal(new Date());
+  constructor() {
+    effect(() => {
+      if (this.active()) {
+        const intervalId = setInterval(() => {
+          this.now.set(new Date());
+          if (!this.active()) { clearInterval(intervalId); }
+        }, 250);
+        return () => clearInterval(intervalId);
+      }
+      
+      return () => this.now.set(new Date());
+    });
+    // effect makes 'now' update continuously while active
+  }
+  
   public startTimer() {
-    console.log("hello");
-    this.times.update(arr => {
-      const copy = [...arr];
-      copy[copy.length - 1].start = Date.now();
-      return copy;
-    })
     this.active.set(true);
-
-    const intervalId = setInterval(() => {
-      this.now.set(Date.now());
-      if (!this.active()) { clearInterval(intervalId); }
-    }, 1000);
+    this.now.set(new Date());
+    this.start.set(new Date());
   }
 
   public timeLeft = computed(() => {
-    const recent = this.times()[this.times().length - 1];
-    if (!recent.start) return recent.remaining;
-
-    return this.now() - (recent.start ?? this.now());
+    const elapsed = this.now().getSeconds() - this.start().getSeconds();
+    return this.duration - elapsed;
   })
-  // problem is its in miliseconds
 }
